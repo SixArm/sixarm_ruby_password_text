7d926721f73146cd1d55cb02a28b8ddfdbf836ca
SixArm <sixarm@sixarm.com>
2010-12-14 23:47:13 -0800

    update readme files

--- lib/sixarm_ruby_password_text.rb
+++ lib/sixarm_ruby_password_text.rb
@@ -1,15 +1,17 @@
+# -*- encoding: utf-8 -*-
 =begin rdoc
 
-= SixArm Ruby Gem: PasswordText class to generate good secure random passwords.
+= SixArm Ruby Gem » PasswordText
 
 Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
 Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
-License:: CreativeCommons License, Non-commercial Share Alike
-License:: LGPL, GNU Lesser General Public License
+License:: Your choice of BSD, MIT, LGPL, or CreativeCommons Non-commercial Share Alike
 
-Generates strong yet user-friendly passwords using Ruby's secure random cryptographic functions.
+PasswordText generates random passwords that are secure and user-friendly.
 
-You can change how passwords are created using the optional parameters and the return value is a string, so you can do string methods on it.
+You can change how passwords are created using the optional parameters.
+
+The return value is a string, so you can do string methods on it.
 
 The default length is 12 characters, which is sufficiently strong for most web applications. You can make this stronger as needed.
 
